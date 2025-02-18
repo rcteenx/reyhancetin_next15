f5a8457fa27ffb89ec31d9fb608bedd65c9a5fe5
“Reyhan <rcteenx@gmail.com>
2025-02-18 20:30:29 +0300

    update home mt-12

--- components/sections/home/1-hero.jsx
+++ components/sections/home/1-hero.jsx
@@ -8,7 +8,7 @@ const Hero = ({ rVideo }) => {
       className="md:container-p max-w-7xl mx-auto my-4 xl:my-8 flex flex-col xl:flex-row justify-center gap-0"
     >
       <div className="xl:w-1/2 xl:pr-2 text-center xl:text-left ">
-        <h2 className="mt-8 font-light text-center xl:text-left text-2xl lg:text-3xl text-primary leading-6">
+        <h2 className="mt-12 font-light text-center xl:text-left text-2xl lg:text-3xl text-primary leading-6">
           <span className="border-b border-indigo xl:border-primary">
             {contHero.h2a}
           </span>
