d0901dc6ae2c9e34653b43a651a1f19580d9570e
“Reyhan <rcteenx@gmail.com>
2025-02-26 14:10:39 +0300

    update home header 2

--- components/ui/Header.jsx
+++ components/ui/Header.jsx
@@ -24,11 +24,14 @@ export default function Header() {
         {/* <div className="w-10 md:w-16 xl:w-20">
           <img src={smd.siteLogo} alt={smd.title} className="p-0.5" />
         </div> */}
-        <div className="px-2 py-0  sm:py-1 bg-slate-100 rounded-xl border">
-          <h3 className="my-1 sm:my-2 font-light text-left text-base md:text-3xl leading-6  ">
+        <h3 className="mt-2 px-2 py-0 font-bold text-left text-base md:text-3xl leading-6  bg-slate-100 rounded-xl border sm:hidden">
+          RÇ
+        </h3>
+        <div className="px-2 py-0 sm:py-1 hidden sm:block ">
+          <h3 className="my-1 sm:my-2 font-semibold text-left text-base md:text-3xl leading-6  ">
             Reyhan Çetin
           </h3>
-          <p className="mt-1 font-light text-2xs sm:text-base tracking-tight hidden sm:block sm:border-t border-indigo">
+          <p className="mt-1 font-light text-2xs sm:text-base tracking-tight sm:border-t border-indigo">
             Bilinç ve Farkındalık Rehberi
           </p>
         </div>
