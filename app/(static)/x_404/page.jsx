97a6a85bad71c06fd3a804039045b21ae1291a7f
“Reyhan <rcteenx@gmail.com>
2025-04-21 12:49:57 +0300

    fix 500 error

--- app/(static)/x_404/page.jsx
+++ app/(static)/x_404/page.jsx
@@ -1,21 +1,21 @@
 import Link from "next/link";
-import { hero404 } from "@/content/data/hero";
+import { hero404 as hero } from "@/content/data/hero";
 
 import HeroSingle from "@/components/templates/hero";
 
 export async function generateMetadata() {
   return {
-    title: { absolute: "Reyhan Çetin X - 404: " + hero404.p },
+    title: { absolute: "Reyhan Çetin X - 404: " + hero.p },
   };
 }
 
 export default function StaticPage() {
   return (
     <>
-      <HeroSingle contHero={hero404}>
-        <p className="my-4 text-indigo text-xs md:text-sm">{hero404.p}.</p>
+      <HeroSingle contHero={hero}>
+        <p className="my-4 text-indigo text-xs md:text-sm">{hero.p}.</p>
         <h4 className="my-4 text-indigo">
-          <Link href="https://www.reyhancetin.org.tr/">{hero404.cta}</Link>
+          <Link href="https://www.reyhancetin.org.tr/">{hero.cta}</Link>
         </h4>
       </HeroSingle>
     </>
--- app/(static)/x_500/page.jsx
+++ app/(static)/x_500/page.jsx
@@ -1,21 +1,21 @@
 import Link from "next/link";
-import { hero500 } from "@/content/data/hero";
+import { hero500 as hero } from "@/content/data/hero";
 
 import HeroSingle from "@/components/templates/hero";
 
 export async function generateMetadata() {
   return {
-    title: { absolute: "Reyhan Çetin X - 500: " + hero500.p },
+    title: { absolute: "Reyhan Çetin X - 500: " + hero.p },
   };
 }
 
 export default function StaticPage() {
   return (
     <>
-      <HeroSingle contHero={hero404}>
-        <p className="my-4 text-indigo text-xs md:text-sm">{hero500.p}</p>
+      <HeroSingle contHero={hero}>
+        <p className="my-4 text-indigo text-xs md:text-sm">{hero.p}</p>
         <h4 className="my-4 text-indigo">
-          <Link href="https://www.reyhancetin.org.tr/">{hero500.cta}</Link>
+          <Link href="https://www.reyhancetin.org.tr/">{hero.cta}</Link>
         </h4>
       </HeroSingle>
     </>
